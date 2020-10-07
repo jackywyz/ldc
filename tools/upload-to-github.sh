4fa1bac138c464034ffa8dee1489e7698e73ae3c
bluewyz <jackywyz@163.com>
2020-10-08 00:35:52 +0800

    Update upload-to-github.sh

--- tools/upload-to-github.sh
+++ tools/upload-to-github.sh
@@ -6,17 +6,17 @@ if [ "$#" -ne 2 ]; then
   echo "Usage: $0 <tag> <file> (and GITHUB_TOKEN environment variable)"
   exit 1
 fi
+if [[ "$CIRRUS_RELEASE" == "" ]]; then
+  echo "Not a release. No need to deploy!"
+  exit 0
+fi
 
-releaseTag=$1
 artifact=$2
 artifactFilename=$(basename $artifact)
-
-releaseID="$(bash -c "curl -s https://api.github.com/repos/jackywyz/ldc/releases/tags/$releaseTag | grep -m 1 '^  \"id\":'")"
-releaseID=${releaseID:8:-1}
-
-echo "Uploading $artifact to GitHub release $releaseTag ($releaseID)..."
+ 
+echo "Uploading $artifact to GitHub release $CIRRUS_TAG ($CIRRUS_RELEASE)..."
 curl -s \
   -H "Authorization: token $GITHUB_TOKEN" \
   -H "Content-Type: application/octet-stream" \
   --data-binary @$artifact \
-  https://uploads.github.com/repos/jackywyz/ldc/releases/$releaseID/assets?name=$artifactFilename
+  https://uploads.github.com/repos/$CIRRUS_REPO_FULL_NAME/releases/$CIRRUS_RELEASE/assets?name=$artifactFilename
